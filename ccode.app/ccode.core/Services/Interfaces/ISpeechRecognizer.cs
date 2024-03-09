da1d86eac57496418b820eb341b62ff79606a29b
b-y-t-e <andrzej.bol@gmail.com>
2024-03-09 22:59:41 +0100

    ISpeechRecognizer

---
+++ ccode.app/ccode.core/Services/Interfaces/ISpeechRecognizer.cs
@@ -0,0 +1,12 @@
+﻿namespace ccode.core.Services.Interfaces;
+
+public interface ISpeechRecognizer
+{
+    IEnumerable<RecognizedSpeech> RecognizeSpeech();
+}
+
+public class RecognizedSpeech
+{
+    public String Text { get; set; }
+    public Boolean HasText { get; set; }
+}
