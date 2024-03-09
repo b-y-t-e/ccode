65803339e5db7b97d69a4b349d820b1c4434c82f
b-y-t-e <andrzej.bol@gmail.com>
2024-03-09 22:56:45 +0100

    options

---
+++ ccode.app/ccode.core/Options/AiCompletionOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace ccode.core.Options;
+
+public class AiCompletionOptions
+{
+    public String Provider { get; set; }
+    public String Key { get; set; }
+    public String Engine { get; set; }
+    public String Endpoint { get; set; }
+}
---
+++ ccode.app/ccode.core/Options/SpeechOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace ccode.core.Options;
+
+public class SpeechOptions
+{
+    public String Provider { get; set; }
+    public String Key { get; set; }
+    public String Region { get; set; }
+}
