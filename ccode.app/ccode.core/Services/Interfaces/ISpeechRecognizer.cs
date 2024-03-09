574890e9af98c301a0c70d0f43f1ab4b1995d812
b-y-t-e <andrzej.bol@gmail.com>
2024-03-09 23:08:40 +0100

    refactor

--- ccode.app/ccode.core/Services/Interfaces/ISpeechRecognizer.cs
+++ ccode.app/ccode.core/Services/Interfaces/ISpeechRecognizer.cs
@@ -1,12 +1,11 @@
-﻿namespace ccode.core.Services.Interfaces;
+﻿using ccode.core.ValueObjects;
+
+namespace ccode.core.Services.Interfaces;
 
 public interface ISpeechRecognizer
 {
     IAsyncEnumerable<RecognizedSpeech> RecognizeSpeech();
 }
 
-public class RecognizedSpeech
-{
-    public String Text { get; set; }
-    public Boolean HasText { get; set; }
-}
+public record RecognizedSpeech(
+    TranscribedSpeech Transcription);
---
+++ ccode.app/ccode.core/Services/Interfaces/ISpeechSynthesizer.cs
@@ -0,0 +1,12 @@
+﻿using ccode.core.ValueObjects;
+
+namespace ccode.core.Services.Interfaces;
+
+public interface ISpeechSynthesizer
+{
+    IAsyncEnumerable<SynthesizedSpeech> SynthesizeSpeech(
+        TranscribedSpeech transcribedSpeech);
+}
+
+public record SynthesizedSpeech(
+    Speech Speech);
