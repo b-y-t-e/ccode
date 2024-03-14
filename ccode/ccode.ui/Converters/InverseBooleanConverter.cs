c0519343d015664376a17c70992a176af1fb0832
b-y-t-e <andrzej.bol@gmail.com>
2024-03-14 22:47:45 +0100

    Create InverseBooleanConverter.cs

---
+++ ccode/ccode.ui/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace ccode.ui.Converters;
+
+public class InverseBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return !boolValue;
+        }
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return !boolValue;
+        }
+        return value;
+    }
+}
